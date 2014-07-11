b8762b15168baa362513efba2790ac4be6a4d707
Camilo Sanchez <camilin87@gmail.com>
2014-07-11 08:20:45 -0400

    pass the batch to checkout as an argument

--- setup-wa2-prod.sh
+++ setup-wa2-prod.sh
@@ -3,12 +3,14 @@
 # IMPORTANT: do not add anything else to this file
 # once rake is installed everything should be run through it
 
+# the first argument of this program if the branch to checkout
+
 sudo apt-get install -y git-core ruby
 yes N | sudo gem install rake
 
 git clone https://camilin87@bitbucket.org/camilin87/wa2.git
 cd wa2
-git checkout develop
+git checkout $1
 git pull
 
 rake configure_pyenv
