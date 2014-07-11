cd3072d4d417b0538bbb9c8cda8743e124319e84
Camilo Sanchez <camilin87@gmail.com>
2014-07-11 07:57:33 -0400

    create setup-wa2-prod.sh

---
+++ setup-wa2-prod.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo apt-get install -y git-core ruby
+yes N | sudo gem install rake
+
+git clone https://camilin87@bitbucket.org/camilin87/wa2.git
+cd wa2 && git pull && git checkout develop
