f443ae1a7d4ea6f9eab38dafd075a01982c38f25
Camilo Sanchez <camilin87@gmail.com>
2014-07-11 08:01:41 -0400

    change directory and get the new branch before pulling

--- setup-wa2-prod.sh
+++ setup-wa2-prod.sh
@@ -4,4 +4,6 @@ sudo apt-get install -y git-core ruby
 yes N | sudo gem install rake
 
 git clone https://camilin87@bitbucket.org/camilin87/wa2.git
-cd wa2 && git pull && git checkout develop
+cd wa2
+git checkout develop
+git pull
